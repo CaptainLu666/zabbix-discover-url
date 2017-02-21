759797a469e2cabb89adb90deacd4d60022b7ea8
mikeluwen <mikeluwen@gamil.com>
2017-02-21 11:06:55 +0800

    change web_site_code_status.sh

--- web_site_code_status.sh
+++ web_site_code_status.sh
@@ -11,13 +11,12 @@ web_site_discovery () {
     for site in ${WEB_SITE[@]}
     do
         num=$(( $num - 1 ))
-    url=$(echo $site|awk '{print $1}')
-    ip=$(echo $site|awk '{print $2}')
+        url=$(echo $site|awk '{print $1}')
+        ip=$(echo $site|awk '{print $2}')
         if [ $num -ne 0 ] ; then
-            printf "\t\t{\"{#SITENAME}\":\""${url}"\",\"{#PROXYIP}\":\""${ip}"\"},\n"
+            printf "\t\t{\"{#SITENAME}\":\""%s"\",\"{#PROXYIP}\":\""${ip}"\"},\n" ${url}
         else
-
-            printf "\t\t{\"{#SITENAME}\":\""${url}"\",\"{#PROXYIP}\":\""${ip}"\"}\n"
+            printf "\t\t{\"{#SITENAME}\":\""%s"\",\"{#PROXYIP}\":\""${ip}"\"}\n" ${url}
             printf '\t]\n'
             printf '}\n'
         fi
@@ -26,11 +25,11 @@ web_site_discovery () {
 
 web_site_code () {
     if [ "$2" == "" ]; then
-        curl -s --connect-timeout 2 -m 4 -o /dev/null -w %{http_code} $1
+        curl -s --connect-timeout 2 -m 4 -o /dev/null -w %{http_code} "$1"
     elif echo $2 |grep ':' &>/dev/null ; then
-        curl -s --connect-timeout 2 -m 4 -o /dev/null -w %{http_code} $1 -x $2
+        curl -s --connect-timeout 2 -m 4 -o /dev/null -w %{http_code} "$1" -x $2
     else
-        curl -s --connect-timeout 2 -m 4 -o /dev/null -w %{http_code} $1 -x $2:80
+        curl -s --connect-timeout 2 -m 4 -o /dev/null -w %{http_code} "$1" -x $2:80
     fi
 }
 
